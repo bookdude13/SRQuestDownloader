e93507055d72945028ea01f00dac63f24f123c04
Ryan Fredlund <rfredlund13@gmail.com>
2023-01-14 13:13:30 -0600

    Able to read from /sdcard/Download/ now

--- Assets/Scripts/DownloadManager.cs
+++ Assets/Scripts/DownloadManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using TMPro;
 
@@ -9,9 +10,28 @@ public class DownloadManager : MonoBehaviour
 
     public void GetAllCustomSongs()
     {
-        displayManager.ResultText.SetText("Getting custom songs...");
+        // displayManager.ResultText.SetText("Getting custom songs...");
         
-        int lastTimeMs = Preferences.GetLastDownloadedTimeMs();
-        displayManager.DebugLog($"Downloading songs after {lastTimeMs}");
+        // int lastTimeMs = Preferences.GetLastDownloadedTimeMs();
+        // displayManager.DebugLog($"Downloading songs after {lastTimeMs}");
+
+        MoveDownloadedCustomSongs();
+    }
+
+    /// Move synth maps and zip files from the Downloads folder on Quest to the CustomSongs directory
+    public void MoveDownloadedCustomSongs() {
+        displayManager.DebugLog($"Moving downloaded customs...");
+        try {
+            displayManager.DebugLog("Download folder exists? " + Directory.Exists("/sdcard/Download/"));
+            var files = Directory.GetFiles("/sdcard/Download/");
+            var zipsSuffix = "synthriderz-beatmaps.zip";
+            displayManager.DebugLog($"{files.Length} files found");
+            var filesEnum = Directory.EnumerateFiles("/sdcard/Download/");
+            foreach (var file in filesEnum) {
+                displayManager.DebugLog("File: " + file);
+            }
+        } catch (System.Exception e) {
+            displayManager.DebugLog("Error! " + e.ToString());
+        }
     }
 }
